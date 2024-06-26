2a0080a8e59631ac639e8ac065308a335191077b
unknown <alishanshaikh9890@gmail.com>
2024-06-26 19:17:05 +0530

    second commit

--- 2. http modules/index.js
+++ 2. http modules/index.js
@@ -1,9 +1,29 @@
 const http = require('http');
 
 const port = 9000;
-const fs = requie('')
+const fs = require('fs');
 const requestHandler = (req, res) => {
-   console.log(req.url)
+   let fileName = "";
+   switch(req.url)
+   {
+    case '/':
+        fileName = "./index.html";
+    break;
+
+    case '/home':
+        fileName = "./home.html";
+    break;
+
+    case '/about':
+        fileName = "./about.html";
+    break;
+   }
+
+   fs.readFile(fileName, (err, result) => {
+    if(!err){
+        res.end(result)
+    }
+   })
 }
 
 const server = http.createServer(requestHandler);
