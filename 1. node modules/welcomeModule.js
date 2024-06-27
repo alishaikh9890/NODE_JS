5d5875c2205c168c167b168cc8544f946aeec7f9
unknown <alishanshaikh9890@gmail.com>
2024-06-27 11:02:10 +0530

    .

--- 1. node modules/welcomeModule.js
+++
@@ -1,9 +0,0 @@
-const welcome = {
-    seyHello : function() {
-                    console.log("Hello Red & White family")
-    },
-    curTime : new Date().toLocaleDateString(),
-    companyName: "red And White Grouop of Institute"
-}
-
-module.exports = welcome;
