ff06335bf889f9f4783e5d71f02a5e3c289f5e22
unknown <alishanshaikh9890@gmail.com>
2024-07-02 18:58:51 +0530

    push

--- 7_MONGODB_CRUD_OPERATION/index.js
+++ 7_MONGODB_CRUD_OPERATION/index.js
@@ -6,10 +6,50 @@ const app = express();
 
 const database = require("./config/database")
 
+const adminTbl = require("./models/adminTbl");
+
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded()); // middleware
+
 app.get("/", (req, res) => {
-    return res.render("form")
+    adminTbl.find({}).then((allData)=> {
+        return res.render("form", {
+        record : allData
+    })
+    }).catch((err) =>{
+            console.log(err);
+            return false
+    })
+   
+})
+
+app.post("/inserData", (req, res) => {
+   const {name, email, password, phone, city} = req.body;
+   adminTbl.create({
+    name : name,
+    email : email,
+    password : password,
+    phone : phone,
+    city : city
+   }).then((data) => {
+    console.log("record successfully insert")
+    return res.redirect("/");
+
+   }).catch((err) => {
+    console.log(err);
+    return false;
+   })
+})
+
+app.get("/deleteData", (req, res)=>{
+    adminTbl.findByIdAndDelete(req.query.id)
+    .then((data)=>{
+        return res.redirect("/")
+    }).catch((err)=>{
+        console.log(err);
+        return false;
+    })
 })
 
 app.listen(port, (err) =>{
---
+++ 7_MONGODB_CRUD_OPERATION/models/adminTbl.js
@@ -0,0 +1,27 @@
+const mongoose  = require("mongoose");
+
+const adminSchema = mongoose.Schema({
+    name: {
+        type : String,
+        required : true
+    },
+    email: {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : String,
+        required : true
+    },
+    city : {
+        type : String,
+        required : true
+    }
+})
+
+const admin = mongoose.model("admin", adminSchema);
+module.exports = admin;
