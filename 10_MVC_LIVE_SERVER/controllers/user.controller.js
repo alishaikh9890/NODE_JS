d988f8a4cd9336b3d1b55862686663d3ae0b4841
Alishan Shaikh <alishanshaikh9890@gmail.com>
2024-07-15 09:54:16 +0530

    from mac in class

--- 10_MVC_LIVE_SERVER/controllers/user.controller.js
+++ 10_MVC_LIVE_SERVER/controllers/user.controller.js
@@ -1,9 +1,12 @@
+const user = require("../models/user.schema")
+
 const Home = (req, res)=>{
     res.send("controllers")
 }
 
-const signup = (req, res)=>{
-    console.log(req.body);
-    res.send("done")
+const signup = async (req, res)=>{
+
+    let data = await user.create(req.body)
+    res.send(data)
 }
-module.exports ={ Home, signup}
+module.exports ={Home, signup}
