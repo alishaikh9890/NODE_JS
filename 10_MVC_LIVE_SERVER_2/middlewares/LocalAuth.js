91df9eff727e48308abccc3922287051e70dc279
Alishan rwbn <alishanshaikh9890@gmail.com>
2024-08-21 19:01:40 +0530

    particle js

--- 10_MVC_LIVE_SERVER_2/middlewares/LocalAuth.js
+++ 10_MVC_LIVE_SERVER_2/middlewares/LocalAuth.js
@@ -3,10 +3,25 @@
 const LocalStrategy = require("passport-local").Strategy;
 
 const LocalAuth = (passport) =>{
-    passport.use(new LocalStrategy(async(username, password, done)=>{
+    passport.use(
+        new LocalStrategy(async(username, password, done)=>{
 
+       let User = await user.findOne({username : username});
 
-    }))
+       if(!User) {
+        return done(null, false)
+       }
+
+       if(User.password != password){
+        return done(null, false);
+       }
+       return done(null, User);
+    })
+ );
+
+ passport.serializeUser((user, done) => {
+    done(null, user.id)
+ })
 }
 
-module.exports = LocalAuth
+module.exports = LocalAuth 
