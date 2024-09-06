4193c9b2d9f904d184c3083c8190e68eb927d7ea
Alishan rwbn <alishanshaikh9890@gmail.com>
2024-09-06 18:18:15 +0530

    complete

--- 10_MVC_LIVE_SERVER_2/routes/routes.js
+++ 10_MVC_LIVE_SERVER_2/routes/routes.js
@@ -1,4 +1,3 @@
-
 const {Router} = require("express");
 const{ Home, signup, update, remove, ui, getsignup, login, Local, LoginPage, profile, logout, forget, resent, resetPage} = require("../controllers/user.controller");
 const {valid, isAuth} = require("../middlewares/user.middleware");
@@ -15,13 +14,13 @@ router.post("/signup", valid, signup);
 
 router.patch("/update/:id", update); 
 
-router.delete("/remove/:id", remove)
+router.delete("/remove/:id", remove) 
 
 router.get("/", ui)
 
 router.get("/signup",getsignup)
 
-router.post("/login", login) 
+router.post("/login", login)
 
 router.post("/local",passport.authenticate("local",{successRedirect: "/user", failureRedirect:"/user/login"}), Local)
 
